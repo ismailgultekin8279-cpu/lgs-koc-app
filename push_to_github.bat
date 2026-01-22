f6592f6e3afa4edd203e6bb27dd0d564606fefbf
LGS Koc <user@example.com>
2026-01-22 18:42:37 +0300

    Fix: Ignore venv and node_modules

---
+++ push_to_github.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Github'a Yukleniyor... Lutfen bekleyin.
+"C:\Program Files\Git\cmd\git.exe" push -u origin main
+echo.
+echo Eger yukarida kirmizi 'fatal' hata varsa okuyun.
+echo "Connect to GitHub" penceresi acildiysa GİRIS YAPIN.
+pause
